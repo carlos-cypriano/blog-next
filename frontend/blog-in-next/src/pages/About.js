34aae42aa2d816becb8a335980c2dcb635168faa
carlos-cypriano <81194143+carlos-cypriano@users.noreply.github.com>
2023-03-27 18:40:33 -0300

    Alterações da vez
    Finalizei a página de sobre do usuário.

--- frontend/blog-in-next/src/pages/About.js
+++ frontend/blog-in-next/src/pages/About.js
@@ -37,7 +37,7 @@ export default function About() {
             <div className='container' style={{ marginTop: '65px' }}>
                 {profiles.filter((p) => p.id == 1).map((profile) => (
                     <div className={'row mt-3'} style={{ background: '#F5FFFA', borderRadius: '15px' }}>
-                    <div className={'col'}>
+                    <div className={'col-md-6'}>
                         <span>
                             <Link href={"/About"} className={"nav-link"}>
                                 <img className={'mt-2'} style={{ borderRadius: '50%', width: '45px', height: '45px' }} src={'../../assets/img/carlos-cypriano.jpg'} alt='profile'></img>
@@ -45,7 +45,25 @@ export default function About() {
                         </span>
                         <span className={'ms-1'}>{profile.username}</span>
 
-                        <div></div>
+                        <div className={'mb-3'}>
+                            <h4 className={'mt-3'}>Name:</h4>
+                            <p>{profile.name}</p>
+                            <h4>User Name:</h4>
+                            <p>{profile.username}</p>
+                            <h4>E-mail:</h4>
+                            <p>{profile.email}</p>
+                        </div>
+                    </div>
+                    <div className={'col-md-6 mt-5'}>
+                        <div className={'mb-3'}>
+                            <h4>Address:</h4>
+                            <p>Street: {profile.address.street}</p>
+                            <p>Suite: {profile.address.suite}</p>
+                            <p>City: {profile.address.city}</p>
+                            <p>Zip-Code: {profile.address.zipcode}</p>
+                            <p>Lat: {profile.address.geo.lat}</p>
+                            <p>Lng: {profile.address.geo.lng}</p>
+                        </div>
                     </div>
                 </div>
                 ))}
